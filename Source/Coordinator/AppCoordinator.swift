05789feb7769f491678c8c0ea91d545539cd1f29
danchopon <dan4111k1@gmail.com>
2019-09-09 14:22:28 +0600

    add AppCoordinator, change AppDelegate

---
+++ Source/Coordinator/AppCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  AppCoordinator.swift
+//  DanStory
+//
+//  Created by Daniyar Erkinov on 9/9/19.
+//  Copyright © 2019 danchopon.com. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    let window: UIWindow?
+    
+    lazy var rootViewController: UINavigationController = {
+        return UINavigationController(rootViewController: UIViewController())
+    }()
+    
+    // MARK: - Coordinator
+    init(window: UIWindow?) {
+        self.window = window
+    }
+    
+    override func start() {
+        guard let window = window else {
+            return
+        }
+        
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
+    
+    override func finish() {
+        
+    }
+    
+}
