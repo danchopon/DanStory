929c3cb32aab701db811cb7c0fa86ae53119b1e1
danchopon <dan4111k1@gmail.com>
2019-09-09 16:38:06 +0600

    add constants file. move uiview helper file

---
+++ Source/Common/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  DanStory
+//
+//  Created by Daniyar Erkinov on 9/9/19.
+//  Copyright © 2019 danchopon.com. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Network {
+        static let splashBaseUrl = "https://api.unsplash.com"
+    }
+    
+}
