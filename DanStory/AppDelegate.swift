356a57048715906f20eec54714674d3284f0853b
danchopon <dan4111k1@gmail.com>
2019-09-09 12:44:04 +0600

    first commit

---
+++ DanStory/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DanStory
+//
+//  Created by Daniyar Erkinov on 9/9/19.
+//  Copyright © 2019 danchopon.com. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        let navController = UINavigationController(rootViewController: UIViewController())
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
