f6b8ea4966407f643e5da9d31e163c7f851b6644
danchopon <dan4111k1@gmail.com>
2019-09-10 15:13:36 +0600

    add User model

---
+++ Source/Models/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  DanStory
+//
+//  Created by Daniyar Erkinov on 9/10/19.
+//  Copyright © 2019 danchopon.com. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id, username, name, bio: String
+    let profileImage: ProfileImage
+    
+    enum CodingKeys: String, CodingKey {
+        case id, username, name, bio
+        case profileImage = "profile_image"
+    }
+}
+
+struct ProfileImage: Codable {
+    let small, medium, large: String
+}
