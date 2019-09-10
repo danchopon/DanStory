7f7700efbf4726ecf47572bfc614a76b16e53abb
danchopon <dan4111k1@gmail.com>
2019-09-10 15:08:05 +0600

    add Photo model

---
+++ Source/Models/Photo.swift
@@ -0,0 +1,26 @@
+//
+//  Photo.swift
+//  DanStory
+//
+//  Created by Daniyar Erkinov on 9/10/19.
+//  Copyright © 2019 danchopon.com. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Decodable {
+    let id: String
+    let width: Int
+    let height: Int
+    let likes: Int
+    let description: String
+    let urls: [URLKind.RawValue: String]
+    
+    enum URLKind: String {
+        case raw
+        case full
+        case regular
+        case small
+        case thumb
+    }
+}
