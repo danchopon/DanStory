482472ecc5e5d5a465dc0b77a16af7052b7d0463
danchopon <dan4111k1@gmail.com>
2019-09-18 17:42:48 +0600

    add context menu item class and uiview extensions

--- Source/Common/UIView + Extensions.swift
+++ Source/Common/UIView + Extensions.swift
@@ -99,3 +99,55 @@ extension UIView {
     }
     
 }
+
+extension UIView {
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius  = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    var fullCircle: Bool {
+        get{
+            return layer.cornerRadius == 0 ? false:true
+        }
+        set {
+            if newValue {
+                layer.cornerRadius = bounds.size.width/2
+            } else {
+                layer.cornerRadius = 0
+            }
+        }
+    }
+    
+    var borderWidth: CGFloat{
+        get {
+            return layer.borderWidth
+        }
+        set{
+            layer.borderWidth = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    var borderColor: UIColor?{
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set{
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    func addDropShadow() {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.6
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 2
+    }
+}
